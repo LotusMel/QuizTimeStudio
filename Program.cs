b2b27ff48f279645ce72ad9cf1b2c18d7f562109
Melva Lang <mjlang74@gmail.com>
2020-03-04 20:15:10 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuizTimeStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Question
+    {
+        //have a list of questions and answers for each
+        private static List<Dictionary<string, string>> QuizTime = new List<Dictionary<string, string>>();
+        
+        static 
+        static void AskQuestion(List<Dictionary<string, string>>)
+        {
+            foreach (Dictionary<string, string> quiz in QuizTime)
+            {
+                foreach (KeyValuePair<string, string> line in quiz)
+                {
+                    string question = line.Key;
+
+                    Console.WriteLine(question);
+
+                }
+
+            }
+        }
+    }
+}
+
